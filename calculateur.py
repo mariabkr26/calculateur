846eea3d4a4fb37feec39ec05e92b6133fd1b18b
mariabkr26 <maria@MacBook-Pro-de-Maria.local>
2026-05-29 23:06:02 +0200

    Ajouter calcul du pourboire

--- calculateur.py
+++ calculateur.py
@@ -4,3 +4,9 @@ def calculer_tva(prix_ht: float) -> float:
 
 prix_ttc = calculer_tva(100)
 print(f"Total TTC: {prix_ttc}")
+
+def calculer_pourboire(montant, taux_pourboire):
+    return montant * taux_pourboire / 100
+
+pourboire = calculer_pourboire(100, 15)
+print(f"Pourboire : {pourboire} €")
