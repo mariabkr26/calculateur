317c8344b2feaa9a8f79b7e0220af25f802574f3
mariabkr26 <maria@MacBook-Pro-de-Maria.local>
2026-05-29 22:55:12 +0200

    Ajouter calculer une remise

--- calculateur.py
+++ calculateur.py
@@ -1,6 +1,13 @@
-def calculer_tva(prix_ht: float) -> float:
     return prix_ht * 1.20
+def calculer_tva(prix_ht: float) -> float:
 
 
 prix_ttc = calculer_tva(100)
-print(f"Total TTC: {prix_ttc}")
+print(f"Total TTC: {prix_ttc}") 
+
+
+def calculer_tva(prix_ht, taux_tva):
+    return prix_ht * 120
+
+prix_ttc = calculer_tva(100)
+print(f"total TTC: {prix_ttc}")
